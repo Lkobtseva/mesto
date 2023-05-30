1e6eb1fa2a3322900de72ed70b4bcef663b768fc
Liza Kobtseva <kobtseva_liza00@mail.ru>
2023-05-30 17:46:16 +0300

    c4

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
 
-const openPopup = document.querySelector('.profile__editButton');
+const openPopup = document.querySelector('.profile__edit-button');
 const closePopup = document.querySelector('.close-icon');
 const popup = document.querySelector('.popup');
-
-openPopup.addEventListener('click', function (e) {
-    e.preventDefault();
-    popup.classList.add('active');
-})
-
-closePopup.addEventListener('click', function (e) {
-    e.preventDefault();
-    popup.classList.remove('active');
-})
-
 let nameInput = document.querySelector('.nameinput');
 let jobInput = document.querySelector('.jobinput');
 
 let editedName = document.querySelector('.profile__title');
 let editedJob = document.querySelector('.profile__subtitle');
+
+openPopup.addEventListener('click', function (e) {
+    popup.classList.add("popup__opened");
+    nameInput.value = editedName.textContent;
+    jobInput.value = editedJob.textContent;
+})
+
+closePopup.addEventListener('click', function (e) {
+    popup.classList.remove("popup__opened");
+})
+
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -27,5 +27,6 @@ function handleFormSubmit(evt) {
 
     editedName.textContent = nameInput.value;
     editedJob.textContent = jobInput.value;
+    
 }
 popup.addEventListener('submit', handleFormSubmit);
