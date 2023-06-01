706dd228f92a609006db780bcf7e44d6195010cd
Liza Kobtseva <kobtseva_liza00@mail.ru>
2023-06-01 19:20:21 +0300

    pr3

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 
 const openPopup = document.querySelector('.profile__edit-button');
-const closePopup = document.querySelector('.close-icon');
+const closePopup = document.querySelector('.popup__close-icon');
 const popup = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__nameinput');
-let jobInput = document.querySelector('.popup__jobinput');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 
 let editedName = document.querySelector('.profile__title');
 let editedJob = document.querySelector('.profile__subtitle');
 
-openPopup.addEventListener('click', function (e) {
-    popup.classList.add("popup__opened");
+openPopup.addEventListener('click', function () {
+    popup.classList.add("popup_opened");
     nameInput.value = editedName.textContent;
     jobInput.value = editedJob.textContent;
 })
 
-closePopup.addEventListener('click', function (e) {
-    popup.classList.remove("popup__opened");
+closePopup.addEventListener('click', function () {
+    popup.classList.remove("popup_opened");
 })
 
 
@@ -27,6 +27,6 @@ function handleFormSubmit(evt) {
 
     editedName.textContent = nameInput.value;
     editedJob.textContent = jobInput.value;
-    popup.classList.remove("popup__opened");
+    popup.classList.remove("popup_opened");
 }
 popup.addEventListener('submit', handleFormSubmit);
