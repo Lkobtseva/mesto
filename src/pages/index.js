984156c839df4c9c2ca9139a5b962e4b2e4f266f
Liza Kobtseva <elizavetakobtsevaa@yandex.ru>
2023-08-28 21:13:33 +0300

    sborka

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-//import './index.css';
+import "./index.css";
 
 import { initialCards, config, profileOpenButton, cardOpenButton } from "../utils/constants.js";
 import Card from "../components/card.js";
